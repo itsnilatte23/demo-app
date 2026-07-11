d81b033908f20847534a52cad30a228a195acb8d
Wei Xinc <chin_wei_xin@s2024.ssts.edu.sg>
2026-07-11 09:44:29 +0800

    q

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("nila")
+            
+            Text("wei xin")
+                .foregroundStyle(.blue)
         }
         .padding()
     }
